8a0144d9ea249a9d514db247a6c44627932ac83c
beneducation29-lgtm <beneducation29@gmail.com>
2026-09-26 10:09:23 +0700

    feat: connect expanded evidence-based content to content engine

--- src/services/contentService.ts
+++ src/services/contentService.ts
@@ -1,17 +1,21 @@
 import { LESSON_CONTENT_BLUEPRINTS, getLessonContentBlueprint } from '../data/contentBlueprints';
 import { aiVideoService } from './aiVideoService';
+import { CONTENT_EXPANSION } from '../data/contentExpansion';
+
+const ALL_BLUEPRINTS = [...LESSON_CONTENT_BLUEPRINTS, ...CONTENT_EXPANSION];
+const getBlueprint = (lessonSlug: string) => ALL_BLUEPRINTS.find((item) => item.lessonSlug === lessonSlug);
 
 export const contentService = {
-  getBlueprint: getLessonContentBlueprint,
-  getAllBlueprints: () => LESSON_CONTENT_BLUEPRINTS,
+  getBlueprint,
+  getAllBlueprints: () => ALL_BLUEPRINTS,
   getByCategory: (category: string) =>
-    LESSON_CONTENT_BLUEPRINTS.filter((item) => item.category === category),
+    ALL_BLUEPRINTS.filter((item) => item.category === category),
   getVideoQueue: () =>
-    LESSON_CONTENT_BLUEPRINTS
+    ALL_BLUEPRINTS
       .map((item) => item.aiVideo)
       .filter((video) => video.status !== 'published'),
   getProductionManifest: (lessonSlug: string) => {
-    const blueprint = getLessonContentBlueprint(lessonSlug);
+    const blueprint = getBlueprint(lessonSlug);
     return blueprint ? aiVideoService.buildManifest(blueprint.aiVideo) : undefined;
   }
 };
