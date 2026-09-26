491828cae6e86e27c5cbf838f35f9a2efc9e8c49
beneducation29-lgtm <beneducation29@gmail.com>
2026-09-26 10:05:25 +0700

    feat: connect content engine to AI video production manifests

--- src/services/contentService.ts
+++ src/services/contentService.ts
@@ -1,4 +1,5 @@
 import { LESSON_CONTENT_BLUEPRINTS, getLessonContentBlueprint } from '../data/contentBlueprints';
+import { aiVideoService } from './aiVideoService';
 
 export const contentService = {
   getBlueprint: getLessonContentBlueprint,
@@ -8,5 +9,9 @@ export const contentService = {
   getVideoQueue: () =>
     LESSON_CONTENT_BLUEPRINTS
       .map((item) => item.aiVideo)
-      .filter((video) => video.status !== 'published')
+      .filter((video) => video.status !== 'published'),
+  getProductionManifest: (lessonSlug: string) => {
+    const blueprint = getLessonContentBlueprint(lessonSlug);
+    return blueprint ? aiVideoService.buildManifest(blueprint.aiVideo) : undefined;
+  }
 };
